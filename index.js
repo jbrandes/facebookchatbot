05c21663c597682af44ab9d342b8403c2f4355ad
Jordan Brandes <jordanbrandes@yahoo.com>
2019-01-07 23:28:47 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+       
